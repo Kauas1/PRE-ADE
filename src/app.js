9ade2f2190dac833fb5545299dfbabc472e44f2e
Kauas1 <kauafidelis7@gmail.com>
2024-10-18 08:52:22 -0300

    Fiz o model de empresa, publicacao e usuario

--- src/app.js
+++ src/app.js
@@ -1,14 +1,26 @@
-import express from "express"
-import cors from "cors"
+import express from "express";
+import cors from "cors";
 
-const app = express()
+import conn from "./config/conn.js";
 
-app.use(cors())
-app.use(express.urlencoded({extended: true}))
-app.use(express.json())
+import Empresa from "./models/empresaModel.js";
+import Usuario from "./models/usuarioModel.js";
+import Publicacao from "./models/publicacaoModel.js";
 
-app.use((req, res)=>{
-    res.status(404).json({err: "Rota não encontrada"})
-})
+const app = express();
+
+
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+conn
+  .sync()
+  .then()
+  .catch((err) => console.error(err));
+
+app.use((req, res) => {
+  res.status(404).json({ err: "Rota não encontrada" });
+});
 
 export default app;
