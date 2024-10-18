bfbe7e26629b146e5b141b835a8ec40f424f1827
Kauas1 <kauafidelis7@gmail.com>
2024-10-18 09:12:52 -0300

    Criei agora o Model de Curtidas e Comentarios e liguei as foreignKeys

--- src/app.js
+++ src/app.js
@@ -6,9 +6,10 @@ import conn from "./config/conn.js";
 import Empresa from "./models/empresaModel.js";
 import Usuario from "./models/usuarioModel.js";
 import Publicacao from "./models/publicacaoModel.js";
+import Curtida from "./models/curtidaModel.js";
+import Comentario from "./models/comentarioModel.js";
 
 const app = express();
-
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
